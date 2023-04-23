893dac03795f2bf538f89c5c507329e307db94df
debruin09 <st.kluedbruin09@gmail.com>
2023-04-23 05:08:36 +0200

    logout using supabase

--- layouts/index.vue
+++ layouts/index.vue
@@ -1,50 +1,73 @@
 <script lang="ts" setup>
-import { useCart } from "~~/stores/cart"
-
-const { getCartItems } = useCart()
-
+import { useCart } from "~~/stores/cart";
+const authClient = useSupabaseAuthClient();
+const { getCartItems } = useCart();
+const logout = async () => {
+  const { error } = await authClient.auth.signOut();
+  if (error) {
+    return alert(error.message);
+  }
+  navigateTo("/introduction");
+};
 </script>
 
 <template>
-    <header class="fixed w-full h-20 text-center justify-center p-4 bg-white z-10 shadow-md">
-        <nav class="h-full flex justify-between space-x-5  text-sm text-[#06113C]  z-10">
-            <div class="flex justify-center self-center h-full items-center">
-                <NuxtLink class=" text-2xl font-bold cursor-pointer" to="/">neighbur</NuxtLink>
-            </div>
+  <header
+    class="fixed w-full h-20 text-center justify-center p-4 bg-white z-10 shadow-md"
+  >
+    <nav
+      class="h-full flex justify-between space-x-5 text-sm text-[#06113C] z-10"
+    >
+      <div class="flex justify-center self-center h-full items-center">
+        <NuxtLink class="text-2xl font-bold cursor-pointer" to="/"
+          >neighbur</NuxtLink
+        >
+      </div>
 
-            <div class="flex justify-center self-center h-full items-center">
-                <NuxtLink to="/order/create" class='group transition-all duration-300 ease-in-out'>
-                    <span
-                        class='mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out'>
-                        create order
-                    </span>
-                </NuxtLink>
-                <NuxtLink to="/cart">
-                    <div class="flex items-center bg-gray-200 rounded-3xl px-2 py-1 ">
-                        <CartIcon />
-                        <p class="text-lg font-semibold">{{ getCartItems() }}</p>
-                    </div>
-                </NuxtLink>
-            </div>
+      <div class="flex justify-center self-center h-full items-center">
+        <NuxtLink
+          to="/order/create"
+          class="group transition-all duration-300 ease-in-out"
+        >
+          <span
+            class="mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out"
+          >
+            create order
+          </span>
+        </NuxtLink>
+        <NuxtLink to="/cart">
+          <div class="flex items-center bg-gray-200 rounded-3xl px-2 py-1">
+            <CartIcon />
+            <p class="text-lg font-semibold">{{ getCartItems() }}</p>
+          </div>
+        </NuxtLink>
+      </div>
 
-            <div class="flex divide-x-2  justify-center self-center h-full items-center ">
-                <NuxtLink to="/account" class='group transition-all duration-300 ease-in-out'>
-                    <span
-                        class='mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out'>
-                        account
-                    </span>
-                </NuxtLink>
+      <div
+        class="flex divide-x-2 justify-center self-center h-full items-center"
+      >
+        <NuxtLink
+          to="/account"
+          class="group transition-all duration-300 ease-in-out"
+        >
+          <span
+            class="mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out"
+          >
+            account
+          </span>
+        </NuxtLink>
 
-                <NuxtLink to="/introduction" class='group transition-all duration-300 ease-in-out'>
-                    <span
-                        class='mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out'>
-                        logout
-                    </span>
-                </NuxtLink>
-            </div>
-        </nav>
-    </header>
-    <slot />
-    <Footer />
+        <div class="group transition-all duration-300 ease-in-out">
+          <span
+            @click="logout"
+            class="mx-2 cursor-pointer text-base font-semibold bg-left-bottom bg-gradient-to-r from-[#FF8C32] to-[#FF8C32] bg-[length:0%_3px] bg-no-repeat group-hover:bg-[length:100%_3px] transition-all duration-500 ease-out"
+          >
+            logout
+          </span>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <slot />
+  <Footer />
 </template>
-
