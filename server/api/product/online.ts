a4371a3ae105f4a81525d03f4e51e4a861124ce6
debruin09 <st.kluedbruin09@gmail.com>
2023-04-23 13:17:11 +0200

    fixed: user online api

--- server/api/product/online.ts
+++ server/api/product/online.ts
@@ -1,7 +1,3 @@
-import productsJson from "~~/data/products.json";
-import usersJson from "~~/data/users.json";
-import { Product, User } from "~~/data/types";
-import { AggData } from "../order/[userId]";
 import { serverSupabaseClient, serverSupabaseUser } from "#supabase/server";
 import { Database } from "~~/types/supabase";
 
@@ -18,27 +14,15 @@ export default defineEventHandler(async (event) => {
     .from("User")
     .select("*")
     .eq("location", userData?.location)
-    .eq("online", true).neq("id", userData?.id);
+    .eq("online", true)
+    .neq("id", userData?.id);
+
+  const ids = onlineNearMe?.map((o) => o.id);
 
   const { data: products } = await client
     .from("Product")
     .select("*")
-    .eq("user_id", serverUser?.id);
-  console.log("This is the data: ", userData?.location);
+    .in("user_product", ids!);
 
-  // const q = getQuery(event);
-
-  // const products: Product[] = productsJson;
-  // const onlineUsers = users.filter(
-  //   (u) => u.online === true && u.location === q.location
-  // );
-  // const onlineProd = products.filter((p) =>
-  //   onlineUsers.map((o) => o.id).includes(p.userId)
-  // );
-  // let r: AggData<Product, User, User>[] = [];
-  // for (let i = 0; i < onlineUsers.length; i++) {
-  //   r.push({ o: onlineProd[i], p: onlineUsers[i] });
-  // }
-  // return r;
-  return onlineNearMe;
+  return products;
 });
