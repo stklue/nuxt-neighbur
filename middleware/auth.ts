9b32e7dd2e4875d1ec098bcc04f26cd6168e4df8
debruin09 <st.kluedbruin09@gmail.com>
2023-04-23 11:53:51 +0200

    auth middle

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
  
   const authClient = useSupabaseAuthClient()
   const isUser = await authClient.auth.getUser()
-  console.log("User", isUser.data);
+  console.log("This is the User", isUser.data);
   
   if (isUser.data == null) {
     return navigateTo("/introduction");
