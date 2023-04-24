efc5197a5d0048349ae12531e7517dadd0f35890
debruin09 <st.kluedbruin09@gmail.com>
2023-04-24 11:32:52 +0200

    footer and other pages

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth",
+  layout: "index",
+});
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen p-20">About us</div>
+</template>
---
+++ pages/privacy.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: "auth",
+  layout: "index",
+});
+
+</script>
+
+<template>
+  <div class="flex justify-center items-center min-h-screen p-20">Privacy Policies</div>
+</template>
