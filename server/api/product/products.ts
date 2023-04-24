98ab41323701bf5dbc52b5db86f83ee187199f71
debruin09 <st.kluedbruin09@gmail.com>
2023-04-24 11:34:19 +0200

    removed trash

--- server/api/product/products.ts
+++
@@ -1,6 +0,0 @@
-import productsJson from "~~/data/products.json" 
-import { Product } from "~~/data/types";
-export default defineEventHandler(() => {
-  const products: Product[] = productsJson;
-  return products;
-});
--- server/api/product/user.ts
+++
@@ -1,8 +0,0 @@
-import productsJson from "~~/data/products.json";
-import { Product } from "~~/data/types";
-export default defineEventHandler((event) => {
-  const q = getQuery(event);
-  const products: Product[] = productsJson;
-
-  return products.filter((p) => p.userId === Number.parseInt(q.id!.toString()));
-});
