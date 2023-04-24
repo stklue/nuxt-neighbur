38502e2ae7cf229f76fb7cd968e5067dd21f20d9
debruin09 <st.kluedbruin09@gmail.com>
2023-04-24 13:17:16 +0200

    payed server route

---
+++ server/api/order/payed.ts
@@ -0,0 +1,18 @@
+import { serverSupabaseClient } from "#supabase/server";
+import { Database } from "~~/types/supabase";
+
+export default defineEventHandler(async (event) => {
+  const client = serverSupabaseClient<Database>(event);
+
+  const q = getQuery(event);
+  const id = Number.parseInt(q.id as string);
+
+  const { data: ordersConfirmed } = await client
+    .from("Order")
+    .select("*, Product(*)")
+    .eq("student_uid", id)
+    .eq("confirmed", "payed")
+    .order("created_at", { ascending: false });
+
+  return ordersConfirmed;
+});
